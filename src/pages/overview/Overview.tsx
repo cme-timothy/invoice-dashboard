9a5bf47e5c2379bbf0ccb97c933eb04e6285f2f9
cme-timothy <timothy.magee@cmeducations.se>
2022-11-21 15:57:53 +0100

    overview page foundation finished

--- src/pages/overview/Overview.tsx
+++ src/pages/overview/Overview.tsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet-async";
+import { Flex, Heading, SimpleGrid, Box } from "@chakra-ui/react";
 
 export default function Overview() {
   return (
@@ -6,7 +7,28 @@ export default function Overview() {
       <Helmet>
         <title>Invoice dashboard - Overview</title>
       </Helmet>
-      <h2>Overview</h2>
+      <Flex direction="column" w="100%" h="100vh">
+        <Flex h="5.5em" bg="white" align="center">
+          <Heading ml="1em">Overview</Heading>
+        </Flex>
+        <SimpleGrid minChildWidth="25em" spacing="1em" m="2em" overflow="auto">
+          <Box bg="white" minH="15em" p="1em">
+            Projects
+          </Box>
+          <Box bg="white" minH="15em" p="1em">
+            Tasks
+          </Box>
+          <Box bg="white" minH="15em" p="1em">
+            Invoices
+          </Box>
+          <Box bg="white" minH="15em" p="1em">
+            Logged time: past 30 days
+          </Box>
+          <Box bg="white" minH="15em" p="1em">
+            Money earned: past year
+          </Box>
+        </SimpleGrid>
+      </Flex>
     </>
   );
 }
