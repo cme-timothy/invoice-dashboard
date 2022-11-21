b937979d6a13391808848826aa2a065692ed784e
cme-timothy <timothy.magee@cmeducations.se>
2022-11-21 16:17:37 +0100

    NoMatch page finsihed

--- src/components/NoMatch.tsx
+++ src/components/NoMatch.tsx
@@ -1,3 +1,20 @@
+import { Helmet } from "react-helmet-async";
+import { Flex, Heading, Box } from "@chakra-ui/react";
+
 export default function NoMatch() {
-  return <h2>No Match</h2>;
+  return (
+    <>
+      <Helmet>
+        <title>Page does not exist!</title>
+      </Helmet>
+      <Flex direction="column" w="100%" h="100vh">
+        <Flex h="5.5em" bg="white" align="center">
+          <Heading p="1em">No Match </Heading>
+        </Flex>
+        <Heading m="1em" bg="white" h="100%" p="1em">
+          Wrong URL: Page does not exist!
+        </Heading>
+      </Flex>
+    </>
+  );
 }
