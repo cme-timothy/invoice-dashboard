8a2ae850ef286e021a0799d59ee9b8c02e9789a7
cme-timothy <timothy.magee@cmeducations.se>
2022-11-24 21:33:23 +0100

    fixed typing error

--- src/hooks/useDataContext.tsx
+++ src/hooks/useDataContext.tsx
@@ -5,7 +5,7 @@ export const useDataContext = () => {
   const contextValues = useContext(DataContext);
 
   if (!contextValues) {
-    throw new Error("useGlobalContext is used outside its context");
+    throw new Error("useDataContext is used outside its context");
   }
 
   return contextValues;
